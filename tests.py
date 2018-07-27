f39eaa238c8b5b64dc3fc730962ba68ac78c3e36
Kamil Kwapisz <you@example.com>
2018-07-27 14:51:05 +0200

    tests module created, plain_text test and file handling test created

--- tests.py
+++ tests.py
@@ -26,4 +26,62 @@ class CarParserTestCase(unittest.TestCase):
         self.assertIsNotNone(test_file, "checking if file is closed after executing close_file method")
         test_file.close()
 
-    def test_
+    def test_translate_dict_keys(self):
+        invalid_dict: dict = {'test': 'testcase'}
+        with self.assertRaises(KeyError) as key_err:
+            translate_dict_keys(invalid_dict)
+
+        the_exception = key_err.exception
+        self.assertEqual(str(the_exception), "Offer does not contain crucial information")
+
+        keys_list = ['make', 'model', 'year', 'mileage', 'petrol_type', 'type', 'no_accidents']
+        valid_dict1 = {
+            'marka_pojazdu': "bmw",
+            'model_pojazdu': "thalia",
+            'rok_produkcji': "1998",
+            'przebieg': "123_456_km",
+            'rodzaj_paliwa': "diesel",
+            'typ': "sedan",
+            'bezwypadkowy': "tak"
+        }
+
+        translated_dict = CarParser.translate_dict_keys(valid_dict1)
+        self.assertListEqual(list(translated_dict.keys()), keys_list)
+        self.assertEqual(translated_dict['make'], "bmw")
+        self.assertEqual(translated_dict['model'], "thalia")
+        self.assertEqual(translated_dict['year'], "1998")
+        self.assertEqual(translated_dict['mileage'], "123456")
+        self.assertEqual(translated_dict['petrol_type'], "diesel")
+        self.assertEqual(translated_dict['type'], "sedan")
+        self.assertEqual(translated_dict['no_accidents'], True)
+
+        valid_dict1 = {
+            'marka_pojazdu': "bmw",
+            'model_pojazdu': "thalia",
+            'rok_produkcji': "1998",
+            'przebieg': "123456",
+            'rodzaj_paliwa': "diesel",
+            'typ': "sedan",
+        }
+
+        translated_dict = CarParser.translate_dict_keys(valid_dict1)
+        self.assertEqual(translated_dict['no_accidents'], False)
+        self.assertEqual(translated_dict['petrol_type'], "diesel")
+
+        valid_dict2 = {
+            'marka_pojazdu': "bmw",
+            'model_pojazdu': "thalia",
+            'rok_produkcji': "1998",
+            'przebieg': "123456",
+            'rodzaj_paliwa': "diesel",
+            'typ': "sedan",
+            'bezwypadkowy': "nie"
+        }
+
+        translated_dict = CarParser.translate_dict_keys(valid_dict1)
+        self.assertEqual(translated_dict['no_accidents'], False)
+
+
+
+
+
